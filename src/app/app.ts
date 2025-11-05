0a399b476c0971d752ac70ab93cff2c58468efea
edav404 <emmanuel.davila@correo.tdea.edu.co>
2025-11-05 01:16:09 -0500

    feature: bottom navbar menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { Navbar } from './pages/navbar/navbar';
+import { NavbarBottomComponent } from './pages/navbar-bottom/navbar-bottom.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, CommonModule, FormsModule],
+  imports: [RouterOutlet, Navbar, NavbarBottomComponent, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 export class App {
- 
+  constructor(public router: Router) {}
+
+  shouldShowNavbarBottom(): boolean {
+    const hiddenRoutes = ['/login', '/registrar'];
+    return !hiddenRoutes.includes(this.router.url);
+  }
 }
